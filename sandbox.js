de8a6c1bd477eecaadc0d54d2303c30e133a7701
Jonathan Randen <jonathanranden@gmail.com>
2023-10-18 10:53:37 +0200

    fixed speed, centerd  and added input field

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,15 @@
 const dynamicText = document.querySelector("h1")
-const speed = document.getElementById('speed').value * 200;
-const words = ["Røyken Videregående Skole"]
+var speed = 500 / document.getElementById("speed").value 
+const words = ["This is an auto typing project",]
+var form = document.getElementById("formId")
+let text = document.getElementById("inputText")
 let Deleting = false 
 let i = 0
 let x = 0
 
 
 const typing = () => {
-
+    var speed = 500 / document.getElementById("speed").value 
     const currentWord = words[x]
     const currentChar = currentWord.substring(0,i);
     dynamicText.textContent = currentChar;
@@ -22,9 +24,23 @@ const typing = () => {
     }
     else {
         Deleting = !Deleting
+        x = !Deleting ? (x + 1) % words.length : x;
         setTimeout(typing,1200)
+    
     }
     }
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+    })
+
+    form.addEventListener("submit", function () {
+        inputString = text.value
+        console.log(inputString)
+        words.push(inputString)
+
+    })
+
 
     
 console.log(speed)
